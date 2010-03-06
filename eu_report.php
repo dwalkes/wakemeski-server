7fedde3b71b83b6cb83b20c6f7c40fe0a390be4a
doanac <doanac@fd9512f8-c594-11dd-9d8b-4f985c3f9f46>
2010-03-06 00:48:28 +0000

    initial support for European ski reports
    This adds the main code to parse ski reports for
    Europe using j2ski.mobi.
    
    Currently, there's only support for Switzerland.
    There are two items still missing:
    
    1) Weather Icon: This will always show up with a "?"
    2) "Map It" doesn't work. I haven't entered the latitude and
       longitude coordinates for each location.
    
    I'll keep making updates. Most of the work going forward with
    this is going to be very tedious.
    
    
    
    git-svn-id: https://android-ski-report.googlecode.com/svn/trunk/php@58 fd9512f8-c594-11dd-9d8b-4f985c3f9f46

---
+++ eu_report.php
@@ -0,0 +1,269 @@
+<?php
+/*
+ * Copyright (c) 2008 nombre.usario@gmail.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *	  derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require_once('weather.inc');
+
+	header( "Content-Type: text/plain" );
+
+	$location = $_GET['location'];
+
+	//first validate the location:
+	if(!get_location_info($location))
+	{
+		print "err.msg=invalid location: $location\n";
+		exit(1);
+	}
+
+	$found_cache = have_cache($location);
+	if( !$found_cache )
+	{
+		write_report($location);
+	}
+
+	print file_get_contents("eu_$location.txt");
+	print "cache.found=$found_cache\n";
+
+function have_cache($location)
+{
+	$file = "eu_$location.txt";
+	if( is_readable($file))
+	{
+		//get modification time stamp. If its less than
+		//120 minutes old, use that copy
+		$mod = filemtime($file);
+		if( time() - $mod < 7200 ) //=60*120 = 120 minutes
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+function write_report($loc)
+{
+	$fp = fopen("eu_$loc.txt", 'w');
+
+	fwrite($fp, "location = $loc\n");
+
+	list($readable, $url) = get_location_info($loc);
+	$report = get_report($url);
+	if( $report )
+	{
+		$props = get_report_props($url, $report);
+		$keys = array_keys($props);
+		for($i = 0; $i < count($keys); $i++)
+		{
+			$key = $keys[$i];
+			fwrite($fp, $key.' = '.$props[$key]."\n");
+		}
+		fwrite($fp, "location.info=$url\n");
+
+/*TODO, change get_location_info to return this
+		fwrite($fp, "location.latitude=$lat\n");
+		fwrite($fp, "location.longitude=$lon\n");
+*/
+	}
+	else
+	{
+		fwrite($fp, "err.msg=No ski report data found\n");
+	}
+
+	fclose($fp);
+}
+
+/**
+ * Takes the report's XML node and parse the information out into a hashtable
+ */
+function get_report_props($url, $report)
+{
+	$props = array();
+
+	preg_match_all("/<th>Upper<\/th><td>(.*?)<\/td>/", $report, $matches, PREG_OFFSET_CAPTURE);
+	$upper = $matches[1][0][0];
+	preg_match_all("/<th>Lower<\/th><td>(.*?)<\/td>/", $report, $matches, PREG_OFFSET_CAPTURE);
+	$lower = $matches[1][0][0];
+	$props['snow.total'] = "Lower ($lower), Upper($upper)";
+
+	preg_match_all("/<th>Fresh Snow<\/th><td>(.*?)<\/td>/", $report, $matches, PREG_OFFSET_CAPTURE);
+	$props['snow.daily'] = $matches[1][0][0]." fresh";
+
+	preg_match_all("/<th>Area Open<\/th><td>(.*?)<\/td>/", $report, $matches, PREG_OFFSET_CAPTURE);
+	$props['trails.total'] = $matches[1][0][0];
+
+	preg_match_all("/<th>Conditions<\/th><td>(.*?)<\/td>/", $report, $matches, PREG_OFFSET_CAPTURE);
+	$props['snow.conditions'] = $matches[1][0][0];
+
+	preg_match_all("/<th>Reported<\/th><td>(.*?)<\/td>/", $report, $matches, PREG_OFFSET_CAPTURE);
+	$props['date'] = date('d M Y', strtotime($matches[1][0][0]));
+
+	preg_match_all("/<p><a href=\"(.*?)\.html/", $report, $matches, PREG_OFFSET_CAPTURE);
+	$page = $matches[1][0][0].".html";
+	//this gives us chamonix_snow-forecast.html, now pull the base from the
+	//report's url
+	$idx = strrpos($url, '/');
+	$base = substr($url, 0, $idx);
+	$props['weather.url'] = $base."/".$page;
+
+	$props['weather.icon'] = get_weather_icon($props['weather.url']);
+
+	return $props;
+}
+
+function get_report($url)
+{
+	return file_get_contents($url);
+}
+
+function get_weather_icon($url)
+{
+    $weather = get_report($url);
+    preg_match_all("/<img alt=\"(.*?)\"/", $weather, $matches, PREG_OFFSET_CAPTURE);
+
+    switch(strtolower($matches[1][0][0]))
+    {
+        case 'sunny/clear':
+            return 'skc';
+        case 'fair': //cloud with sun
+            return 'sct';
+        case 'light snow':
+            return 'mix';
+        case 'snow':
+        case 'heavy snow':
+            return 'blizzard';
+        case 'cloudy':
+            return 'ovc';
+        case 'partly cloudy': //little sun most cloud
+            return 'bkn';
+    }
+    return $matches[1][0][0];
+}
+
+/**
+ * Turns a 4 digit code like fchm and returns a tuple
+ * (Chamonix, http://www.j2ski.mobi/france/chamonix_snow-report.html)
+ */
+function get_location_info($loc)
+{
+//switzerland
+	if( $loc == 'seng')
+		return array('Engelberg', 'http://www.j2ski.mobi/switzerland/engelberg_snow-report.html');
+	if( $loc == 'sand' )
+		return array('Andermatt', 'http://www.j2ski.mobi/switzerland/andermatt_snow-report.html');
+	if( $loc == 'sanz' )
+		return array('Anzère', 'http://www.j2ski.mobi/switzerland/anzere_snow-report.html');
+	if( $loc == 'slax' )
+		return array('Laax', 'http://www.j2ski.mobi/switzerland/laax_snow-report.html');
+	if( $loc == 'sflf' )
+		return array('Flims-Laax-Falera', 'http://www.j2ski.mobi/switzerland/flims_laax_falera_snow-report.html');
+	if( $loc == 'sssf' )
+		return array('Saas-Fee', 'http://www.j2ski.mobi/switzerland/saas_fee_snow-report.html');
+	if( $loc == 'slkd' )
+		return array('Leukerbad', 'http://www.j2ski.mobi/switzerland/leukerbad_snow-report.html');
+	if( $loc == 'smur' )
+		return array('Mürren', 'http://www.j2ski.mobi/switzerland/murren_snow-report.html');
+	if( $loc == 'spot' )
+		return array('Pontresina', 'http://www.j2ski.mobi/switzerland/pontresina_snow-report.html');
+	if( $loc == 'sslv' )
+		return array('Silvaplana', 'http://www.j2ski.mobi/switzerland/silvaplana_snow-report.html');
+	if( $loc == 'sstm' )
+		return array('St. Moritz', 'http://www.j2ski.mobi/switzerland/st_moritz_snow-report.html');
+	if( $loc == 'szrm' )
+		return array('Zermatt', 'http://www.j2ski.mobi/switzerland/zermatt_snow-report.html');
+	if( $loc == 'smhg' )
+		return array('Meiringen - Hasliberg', 'http://www.j2ski.mobi/switzerland/meiringen_hasliberg_snow-report.html');
+	if( $loc == 'snen' )
+		return array('Nendaz', 'http://www.j2ski.mobi/switzerland/nendaz_snow-report.html');
+	if( $loc == 'svey' )
+		return array('Veysonnaz', 'http://www.j2ski.mobi/switzerland/veysonnaz_snow-report.html');
+	if( $loc == 'scrm' )
+		return array('Crans-Montana', 'http://www.j2ski.mobi/switzerland/crans_montana_snow-report.html');
+	if( $loc == 'sflm' )
+		return array('Flumserberg', 'http://www.j2ski.mobi/switzerland/flumserberg_snow-report.html');
+	if( $loc == 'sgrm' )
+		return array('Grimentz', 'http://www.j2ski.mobi/switzerland/grimentz_snow-report.html');
+	if( $loc == 'slnv' )
+		return array('Lenzerheide - Valbella', 'http://www.j2ski.mobi/switzerland/lenzerheide_valbella_snow-report.html');
+	if( $loc == 'sldb' )
+		return array('Les Diablerets', 'http://www.j2ski.mobi/switzerland/les_diablerets_snow-report.html');
+	if( $loc == 'stlc' )
+		return array('Thyon les Collons', 'http://www.j2ski.mobi/switzerland/thyon_les_collons_snow-report.html');
+	if( $loc == 'sgst' )
+		return array('Gstaad', 'http://www.j2ski.mobi/switzerland/gstaad_snow-report.html');
+	if( $loc == 'sdsr' )
+		return array('Disentis Sedrun', 'http://www.j2ski.mobi/switzerland/disentis_sedrun_snow-report.html');
+	if( $loc == 'sverb' )
+		return array('Verbier', 'http://www.j2ski.mobi/switzerland/verbier_snow-report.html');
+	if( $loc == 'sltz' )
+		return array('La Tzoumaz', 'http://www.j2ski.mobi/switzerland/la_tzoumaz_snow-report.html');
+	if( $loc == 'salt' )
+		return array('Aletsch', 'http://www.j2ski.mobi/switzerland/aletsch_snow-report.html');
+	if( $loc == 'sdav' )
+		return array('Davos', 'http://www.j2ski.mobi/switzerland/davos_snow-report.html');
+	if( $loc == 'smrg' )
+		return array('Morgins', 'http://www.j2ski.mobi/switzerland/morgins_snow-report.html');
+	if( $loc == 'sasa' )
+		return array('Arosa', 'http://www.j2ski.mobi/switzerland/arosa_snow-report.html');
+	if( $loc == 'skls' )
+		return array('Klosters', 'http://www.j2ski.mobi/switzerland/klosters_snow-report.html');
+	if( $loc == 'smin' )
+		return array('Minschuns', 'http://www.j2ski.mobi/switzerland/minschuns_snow-report.html');
+	if( $loc == 'ssam' )
+		return array('Samnaun', 'http://www.j2ski.mobi/switzerland/samnaun_snow-report.html');
+	if( $loc == 'ssed' )
+		return array('Sedrun', 'http://www.j2ski.mobi/switzerland/sedrun_snow-report.html');
+	if( $loc == 'sstl' )
+		return array('St-Luc / Chandolin', 'http://www.j2ski.mobi/switzerland/st_luc_chandolin_snow-report.html');
+	if( $loc == 'sadl' )
+		return array('Adelboden', 'http://www.j2ski.mobi/switzerland/adelboden_snow-report.html');
+	if( $loc == 'sevo' )
+		return array('Evolène', 'http://www.j2ski.mobi/switzerland/evolene_snow-report.html');
+    if( $loc == 'swen' )
+		return array('Wengen', 'http://www.j2ski.mobi/switzerland/wengen_snow-report.html');
+    if( $loc == 'skan' )
+		return array('Kandersteg', 'http://www.j2ski.mobi/switzerland/kandersteg_snow-report.html');
+    if( $loc == 'sgrn' )
+		return array('Grindelwald', 'http://www.j2ski.mobi/switzerland/grindelwald_snow-report.html');
+    if( $loc == 'svgn' )
+		return array('Villars - Gryon', 'http://www.j2ski.mobi/switzerland/villars_gryon_snow-report.html');
+    if( $loc == 'sbwd' )
+		return array('Braunwald', 'http://www.j2ski.mobi/switzerland/braunwald_snow-report.html');
+
+//france
+	if( $loc == 'fchm')
+		return array('Chamonix', 'http://www.j2ski.mobi/france/chamonix_snow-report.html');
+
+	return null;
+}
+/*
+Missing Swiss resorts:
+Champéry, Leysin, Zinal, Grächen, Savognin
+Scuol, Château d'Oex, Lenk, Arolla, Bruson, Riederalp
+Les Crosets, Ovronnaz, Charmey, Les Mosses, Rougemont
+Saas Grund, Saas Almagell, Flims, Falera, Brig, Torgon
+*/
+?>
--- location_finder.php
+++ location_finder.php
@@ -7,12 +7,12 @@
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ *	  notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the distribution.
  * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ *	  derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -38,12 +38,14 @@
 
 function show_regions()
 {
-    print "Colorado\n";
+	print "Colorado\n";
 	print "New Mexico\n";
 	print "Montana\n";
 	print "Oregon\n";
 	print "Utah\n";
 	print "Washington\n";
+	print "France\n";
+	print "Switzerland\n";
 }
 
 function show_locations($region)
@@ -54,7 +56,7 @@ function show_locations($region)
 	{
 		print "Alpental = $URL_BASE/nwac_report.php?location=OSOALP\n";
 		print "Crystal Mountain = $URL_BASE/nwac_report.php?location=OSOCMT\n";
-		print "Hurricane Ridge  = $URL_BASE/nwac_report.php?location=OSOHUR\n";
+		print "Hurricane Ridge = $URL_BASE/nwac_report.php?location=OSOHUR\n";
 		print "Mission Ridge = $URL_BASE/nwac_report.php?location=OSOMSR\n";
 		print "Mt Baker = $URL_BASE/nwac_report.php?location=OSOMTB\n";
 		print "Stevens Pass = $URL_BASE/nwac_report.php?location=OSOSK9\n";
@@ -64,7 +66,7 @@ function show_locations($region)
 	else if( $region == "Oregon")
 	{
 		print "Ski Bowl Ski Area, Government Camp = $URL_BASE/nwac_report.php?location=OSOGVT\n";
-		print "Mt Hood Meadows  = $URL_BASE/nwac_report.php?location=OSOMHM\n";
+		print "Mt Hood Meadows = $URL_BASE/nwac_report.php?location=OSOMHM\n";
 		print "Timberline = $URL_BASE/nwac_report.php?location=OSOTIM\n";
 	}
 	else if ( $region == "Utah" )
@@ -96,38 +98,38 @@ function show_locations($region)
 		print "Taos = $URL_BASE/nm_report.php?location=TS\n";
 		print "Valles Caldera Nordic = $URL_BASE/nm_report.php?location=VC\n";
 	}
-    else if ( $region == "Colorado" )
+	else if ( $region == "Colorado" )
 	{
 		print "Arapahoe Basin = $URL_BASE/co_report.php?location=AB\n";
-        print "Aspen Highlands = $URL_BASE/co_report.php?location=AH\n";
-        print "Aspen Mountain = $URL_BASE/co_report.php?location=AM\n";
-        print "Buttermilk = $URL_BASE/co_report.php?location=BM\n";
-        print "Copper Mountain = $URL_BASE/co_report.php?location=CM\n";
-        print "Crested Butte = $URL_BASE/co_report.php?location=CB\n";
-        print "Echo Mountain = $URL_BASE/co_report.php?location=EM\n";
-        print "Eldora = $URL_BASE/co_report.php?location=EL\n";
-        print "Howelsen = $URL_BASE/co_report.php?location=HW\n";
-        print "Loveland = $URL_BASE/co_report.php?location=LV\n";
-        print "Monarch Mountain = $URL_BASE/co_report.php?location=MM\n";
-        print "Powderhorn = $URL_BASE/co_report.php?location=PH\n";
-        print "Purgatory = $URL_BASE/co_report.php?location=PG\n";
-        print "Silverton Mountain = $URL_BASE/co_report.php?location=SM\n";
-        print "Ski Cooper = $URL_BASE/co_report.php?location=SC\n";
-        print "Snowmass = $URL_BASE/co_report.php?location=SN\n";
-        print "Sol Vista Basin = $URL_BASE/co_report.php?location=SV\n";
-        print "Steamboat = $URL_BASE/co_report.php?location=ST\n";
-        print "Sunlight = $URL_BASE/co_report.php?location=SL\n";
-        print "Telluride = $URL_BASE/co_report.php?location=TD\n";
-        print "Winter Park = $URL_BASE/co_report.php?location=WP\n";
-        print "Wolf Creek = $URL_BASE/co_report.php?location=WC\n";
+		print "Aspen Highlands = $URL_BASE/co_report.php?location=AH\n";
+		print "Aspen Mountain = $URL_BASE/co_report.php?location=AM\n";
+		print "Buttermilk = $URL_BASE/co_report.php?location=BM\n";
+		print "Copper Mountain = $URL_BASE/co_report.php?location=CM\n";
+		print "Crested Butte = $URL_BASE/co_report.php?location=CB\n";
+		print "Echo Mountain = $URL_BASE/co_report.php?location=EM\n";
+		print "Eldora = $URL_BASE/co_report.php?location=EL\n";
+		print "Howelsen = $URL_BASE/co_report.php?location=HW\n";
+		print "Loveland = $URL_BASE/co_report.php?location=LV\n";
+		print "Monarch Mountain = $URL_BASE/co_report.php?location=MM\n";
+		print "Powderhorn = $URL_BASE/co_report.php?location=PH\n";
+		print "Purgatory = $URL_BASE/co_report.php?location=PG\n";
+		print "Silverton Mountain = $URL_BASE/co_report.php?location=SM\n";
+		print "Ski Cooper = $URL_BASE/co_report.php?location=SC\n";
+		print "Snowmass = $URL_BASE/co_report.php?location=SN\n";
+		print "Sol Vista Basin = $URL_BASE/co_report.php?location=SV\n";
+		print "Steamboat = $URL_BASE/co_report.php?location=ST\n";
+		print "Sunlight = $URL_BASE/co_report.php?location=SL\n";
+		print "Telluride = $URL_BASE/co_report.php?location=TD\n";
+		print "Winter Park = $URL_BASE/co_report.php?location=WP\n";
+		print "Wolf Creek = $URL_BASE/co_report.php?location=WC\n";
 
-        print "Vail = $URL_BASE/co_report2.php?location=VA\n";
-        print "Beaver Creek = $URL_BASE/co_report2.php?location=BC\n";
-        print "Keystone = $URL_BASE/co_report2.php?location=KS\n";
-        print "Breckenridge = $URL_BASE/co_report2.php?location=BK\n";
-    }
-    else if( $region == "Montana" )
-    {
+		print "Vail = $URL_BASE/co_report2.php?location=VA\n";
+		print "Beaver Creek = $URL_BASE/co_report2.php?location=BC\n";
+		print "Keystone = $URL_BASE/co_report2.php?location=KS\n";
+		print "Breckenridge = $URL_BASE/co_report2.php?location=BK\n";
+	}
+	else if( $region == "Montana" )
+	{
 		print "Turner Mountain = $URL_BASE/mt_report.php?location=TR\n";
 		print "Maverick Mountain= $URL_BASE/mt_report.php?location=MM\n";
 		print "Lost Trail Powder Mountain= $URL_BASE/mt_report.php?location=LM\n";
@@ -143,7 +145,55 @@ function show_locations($region)
 		print "Red Lodge= $URL_BASE/mt_report.php?location=RL\n";
 		print "Bridger Bowl= $URL_BASE/mt_report.php?location=BB\n";
 		print "Montana Snowbowl= $URL_BASE/mt_report.php?location=MS\n";
-		print "Moonlight Basin= $URL_BASE/mt_report.php?location=MB\n";		
-    }
+		print "Moonlight Basin= $URL_BASE/mt_report.php?location=MB\n";
+	}
+	else if( $region == "France" )
+	{
+		print "Engelberg = $URL_BASE/eu_report.php?location=seng\n";
+	}
+	else if( $region == "Switzerland" )
+	{
+		print "Adelboden = $URL_BASE/eu_report.php?location=sadl\n";
+		print "Aletsch = $URL_BASE/eu_report.php?location=salt\n";
+		print "Andermatt = $URL_BASE/eu_report.php?location=sand\n";
+		print "Anzère = $URL_BASE/eu_report.php?location=sanz\n";
+		print "Arosa = $URL_BASE/eu_report.php?location=sasa\n";
+		print "Braunwald = $URL_BASE/eu_report.php?location=sbwd\n";
+		print "Crans-Montana = $URL_BASE/eu_report.php?location=scrm\n";
+		print "Davos = $URL_BASE/eu_report.php?location=sdav\n";
+		print "Disentis Sedrun = $URL_BASE/eu_report.php?location=sdsr\n";
+		print "Engelberg = $URL_BASE/eu_report.php?location=seng\n";
+		print "Evolène = $URL_BASE/eu_report.php?location=sevo\n";
+		print "Flims-Laax-Falera = $URL_BASE/eu_report.php?location=sflf\n";
+		print "Flumserberg = $URL_BASE/eu_report.php?location=sflm\n";
+		print "Grimentz = $URL_BASE/eu_report.php?location=sgrm\n";
+		print "Grindelwald = $URL_BASE/eu_report.php?location=sgrn\n";
+		print "Gstaad = $URL_BASE/eu_report.php?location=sgst\n";
+		print "Kandersteg = $URL_BASE/eu_report.php?location=skan\n";
+		print "Klosters = $URL_BASE/eu_report.php?location=skls\n";
+		print "Laax = $URL_BASE/eu_report.php?location=slax\n";
+		print "La Tzoumaz = $URL_BASE/eu_report.php?location=sltz\n";
+		print "Lenzerheide - Valbella = $URL_BASE/eu_report.php?location=slnv\n";
+		print "Leukerbad = $URL_BASE/eu_report.php?location=slkd\n";
+		print "Les Diablerets = $URL_BASE/eu_report.php?location=sldb\n";
+		print "Meiringen = $URL_BASE/eu_report.php?location=smhg\n";
+		print "Minschuns = $URL_BASE/eu_report.php?location=smin\n";
+		print "Morgins = $URL_BASE/eu_report.php?location=smrg\n";
+		print "Mürren = $URL_BASE/eu_report.php?location=smur\n";
+		print "Nendaz = $URL_BASE/eu_report.php?location=snen\n";
+		print "Pontresina = $URL_BASE/eu_report.php?location=spot\n";
+		print "Saas-Fee = $URL_BASE/eu_report.php?location=sssf\n";
+		print "Samnaun = $URL_BASE/eu_report.php?location=ssam\n";
+		print "Sedrun = $URL_BASE/eu_report.php?location=ssed\n";
+		print "Silvaplana = $URL_BASE/eu_report.php?location=sslv\n";
+		print "St-Luc / Chandolin = $URL_BASE/eu_report.php?location=sstl\n";
+		print "St. Moritz = $URL_BASE/eu_report.php?location=sstm\n";
+		print "Thyon les Collons = $URL_BASE/eu_report.php?location=stlc\n";
+		print "Verbier = $URL_BASE/eu_report.php?location=sverb\n";
+		print "Veysonnaz = $URL_BASE/eu_report.php?location=svey\n";
+		print "Villars - Gryon = $URL_BASE/eu_report.php?location=svgn\n";
+		print "Wengen = $URL_BASE/eu_report.php?location=swen\n";
+		print "Zermatt = $URL_BASE/eu_report.php?location=szrm\n";
+	}
 }
 ?>
