b85dad8207b46c9aee209b83537554b1f0cd6653
Andy Doan <doanac@gmail.com>
2010-11-21 14:41:08 -0800

    make hit_stats.php a little more readable
    This applies some CSS rules to help make the tables a little
    easier to read.
    
    Adds a "usage" section to explain different page parameters.
    
    Adds ability to show "active" users

--- hit_stats.php
+++ hit_stats.php
@@ -1,5 +1,9 @@
 <?php
 
+$ACTIVE_THRESHOLD=12;
+if( $_GET['active'] ) 
+	$ACTIVE_THRESHOLD=$_GET['active'];
+
 function create_hit($line)
 {
 	$parts = preg_split("/\t/", $line);
@@ -92,40 +96,87 @@ arsort($locations);
 ?>
 <html>
 <head>
-	<title>Hit Statistics</title>
+<title>Hit Statistics</title>
+
+<style type="text/css">
+h2 {
+	background-color: #C6DEFF;
+	border-top: 1px solid blue;
+	color: black;
+	padding-left: 5px;
+}
+table {
+	border-collapse: collapse;
+	border: 1px solid blue;
+}
+tr {
+	border-bottom: 1px solid blue;
+}
+tr.title {
+	background-color: #C6DEFF;
+	color: black;
+	text-align: left;
+}
+th {
+	padding-left: 5px;
+}
+td {
+	padding-left: 5px;
+	padding-right: 10px;
+	padding-bottom: 5px;
+}
+</style>
 </head>
 <body>
 
-<h2>Pages Hit: <?php print(count($pages)) ?></h2>
+<h2>Page Usage</h2>
+<p>There are two parameters that can be passed to this page using the HTTP "get" values
+<ul>
+	<li><b>ignoreNA=y: </b>Ignores hits from automated code testing like test.sh</li>
+	<li><b>active=X: </b>Allows you to configure how many hits from a unique ID are required to be considerderd "active"</li>
+</ul>
+For example:<br/>
+<pre>hit_stats.php?ignoreNA=y&active=20</pre>
+Will display stats not counting "real" users. A user will only be counted as active if they've had more than 20 hits.
+</p>
+
+<h2>Pages Hit: (<?php print(count($pages)) ?> Unique)</h2>
 <table>
-	<tr><th>Page</th><th>Hits</th></tr>
+	<tr class="title"><th>Page</th><th>Hits</th></tr>
 <?php
 	foreach($pages as $page=>$hits)
 		print("<tr><td>$page</td><td>$hits</td></tr>\n"); 
 ?>
 </table>
 
-<h2>Location Finder Hits: <?php print(count($locations)) ?></h2>
+<h2>Location Finder Hits: (<?php print(count($locations)) ?> Regions)</h2>
 <table>
-	<tr><th>Location</th><th>Hits</th></tr>
+	<tr class="title"><th>Location</th><th>Hits</th></tr>
 <?php
 	foreach($locations as $loc=>$hits)
 		print("<tr><td>$loc</td><td>$hits</td></tr>\n"); 
 ?>
 </table>
 
-<h2>Resort Hits: <?php print(count($resorts)) ?></h2>
+<h2>Resort Hits: (<?php print(count($resorts)) ?> Resorts)</h2>
 <table>
-	<tr><th>Resort</th><th>Hits</th></tr>
+	<tr class="title"><th>Resort</th><th>Hits</th></tr>
 <?php
 	foreach($resorts as $resort=>$hits)
 		print("<tr><td>$resort</td><td>$hits</td></tr>\n"); 
 ?>
 </table>
 
-<h2>Unique Users: <?php print(count($ids)) ?></h2>
+<?php
+	$active = 0;
+	foreach($ids as $user=>$hits) {
+		if( $hits > $ACTIVE_THRESHOLD )
+			$active++;
+	}
+?>
+<h2>Unique Users: <?php print(count($ids))?>, Active Users: <?php print($active)?></h2>
 <table>
-	<tr><th>ID</th><th>Hits</th></tr>
+	<tr class="title"><th>ID</th><th>Hits</th></tr>
 <?php
 	foreach($ids as $user=>$hits)
 		print("<tr><td>$user</td><td>$hits</td></tr>\n"); 
