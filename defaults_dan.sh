164ce3bee77397823b4bed913e105639777c700b
Dan Walkes <dan@dan-laptop.(none)>
2010-11-22 21:46:22 -0700

    Fix montana fresh snow
    Handle cases where overnight snow is not specified
    and handle cases where overnight snow total is 0.

--- defaults_dan.sh
+++ defaults_dan.sh
@@ -2,6 +2,6 @@
 
 USER=ddubtech
 SERVER=ddubtech.com
-LOC=public_html/wakemeski/skireport
+LOC=public_html/wakemeski/skireport/test
 RECIPIENTS=danwalkes@gmail.com
 SERVER_URL="http://ddubtech.com/wakemeski/skireport"
