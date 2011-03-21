87b929ba22b100ef4cea8645570caec583ea852c
Andy Doan <doanac@gmail.com>
2011-03-20 22:30:37 -0700

    add a test harness for running scripts locally
    This script allows you to run a test locally

---
+++ test_resort.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Copyright (c) 2011 Andy Doan, Dan Walkes
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+if( $argc != 3 )
+{
+	print "Usage: test_resport.php <resort.php> <location code>\n";
+	print "\n";
+	print " Runs a test resort script from the CLI tricking it into\n";
+	print " thinking its run within a webserver\n";
+	exit(1);
+}
+
+$script = $argv[1];
+$location = $argv[2];
+
+$_GET['location'] = $location;
+$_GET['nocache'] = 1; //never use cache
+
+require_once($script);
+?>
