57febc022d186fc3b2f0d109b0293c6e81623411
Andy Doan <doanac@gmail.com>
2011-11-09 00:22:42 -0600

    convert co_report to ReportBase

--- co_report.php
+++ co_report.php
@@ -27,129 +27,131 @@
  */
 
 require_once('co.inc');
+require_once('reportbase.inc');
 
-header( "Content-Type: text/plain" );
-
-	$location = $_GET['location'];
-
-	$resorts = resorts_co_get();
-	$resort = resort_get_location($resorts, $location);
-
-	$resort->fresh_source_url = "http://feeds.feedburner.com/snowreport";
-
-	$cache_file = 'co_'.$location.'.txt';
-	$found_cache = cache_available($resort,$cache_file);
-	if( !$found_cache )
-	{
-		write_report($resort, $cache_file);
-	}
-
-	cache_dump($cache_file, $found_cache);
-
-	log_hit('co_report.php', $location, $found_cache);
-
-function write_report($resort, $cache_file)
+class COReport extends ReportBase
 {
-	$xml = get_location_report($resort);
-	if( $xml )
+	public function run($location)
 	{
-		$props = get_report_props($xml);
-		cache_create($resort, $cache_file, $props);
-	}
-}
+		$resorts = resorts_co_get();
+		$resort = resort_get_location($resorts, $location);
 
-/**
- * Takes the report's XML node and parse the information out into a hashtable
- */
-function get_report_props($report)
-{
-	$props = array();
-	$data = $report->getElementsByTagName('description')->item(0)->nodeValue;
+		$resort->fresh_source_url = "http://feeds.feedburner.com/snowreport";
 
-	$props['snow.daily'] = 'n/a';
-	$props['snow.fresh'] = 'n/a';
-
-	$day = find_int("/New Snow Last 24 hours: (\d+)/", $data);
-	$yesterday = find_int("/New Snow Last 48 hours: (\d+)/", $data);
-
-	if( $day != 'n/a' )
-	{
-		$props['snow.daily'] = "Fresh($day)";
-		$props['snow.fresh'] = $day;
-	}
-
-	if( $yesterday != 'n/a' && $day != 'n/a' )
-		$props['snow.daily'] .= " 48hr($yesterday)";
-	else if( $yesterday != 'n/a' )
-		$props['snow.daily'] = "48hr($yesterday)";
-
-	$props['snow.units'] = 'inches';
-
-	$props['snow.total'] = find_int("/Mid Mountain Depth: (\d+)/", $data);
-
-	preg_match_all("/Lifts Open: (\d+)\/(\d+)/", $data, $matches, PREG_OFFSET_CAPTURE);
-	if($matches[1][0][0])
-	{
-		$props['lifts.open'] = $matches[1][0][0];
-		$props['lifts.total'] = $matches[2][0][0];
-	}
-	else
-	{
-		preg_match_all("/Lifts Open: (\d+)/", $data, $matches, PREG_OFFSET_CAPTURE);
-		if($matches[1][0][0])
-			$props['lifts.open'] = $matches[1][0][0];
-	}
-
-	preg_match_all("/Surface Conditions: (.*?)<br/", $data, $matches, PREG_OFFSET_CAPTURE);
-	if( $matches[1][0][0] )
-		$props['snow.conditions'] = $matches[1][0][0];
-
-	preg_match_all("/Comments:\s+(.*)/", $data, $matches, PREG_OFFSET_CAPTURE);
-	if( $matches[1][0][0] )
-	{
-		$props['location.comments'] = strip_tags($matches[1][0][0]);
-		//ensure we don't just give an empty comment
-		if( $props['location.comments'] == '--' )
-			unset($props['location.comments']);
-	}
-
-	$date = $report->getElementsByTagName('pubDate')->item(0)->nodeValue;
-	$date = strtotime($date);
-	$props['date'] = date("h:ia M j", $date);
-
-	$props['details.url'] = $report->getElementsByTagName('link')->item(0)->nodeValue;
-
-	return $props;
-}
-
-/**
- * Returns the XML node containing the report for a given location or false
- * if one is not found
- */
-function get_location_report($resort)
-{
-	$dom = get_report_xml($resort);
-
-	$items = $dom->getElementsByTagName('item');
-	for ($i = 0; $i < $items->length; $i++)
-	{
-		$title_node = $items->item($i)->getElementsByTagName('title')->item(0);
-		$title = trim($title_node->firstChild->nodeValue);
-		$name = $resort->name;
-		if(preg_match("/$name/", $title) )
+		$cache_file = 'co_'.$location.'.txt';
+		$found_cache = cache_available($resort,$cache_file);
+		if( !$found_cache )
 		{
-		    return $items->item($i);
+			$this->write_report($resort, $cache_file);
 		}
+
+		cache_dump($cache_file, $found_cache);
+
+		log_hit('co_report.php', $location, $found_cache);
 	}
 
-	return false;
-}
+	function get_report($resort)
+	{
+		$xml = self::get_location_report($resort);
+		if( $xml )
+			return self::get_report_props($xml);
+		return array();
+	}
 
-function get_report_xml($resort)
-{
-	$xml = file_get_contents($resort->fresh_source_url);
-	$sxe = simplexml_load_string($xml);
-	return dom_import_simplexml($sxe);
-}
+	/**
+	 * Takes the report's XML node and parse the information out into a hashtable
+	 */
+	static function get_report_props($report)
+	{
+		$props = array();
+		$data = $report->getElementsByTagName('description')->item(0)->nodeValue;
 
+		$props['snow.daily'] = 'n/a';
+		$props['snow.fresh'] = 'n/a';
+
+		$day = find_int("/New Snow Last 24 hours: (\d+)/", $data);
+		$yesterday = find_int("/New Snow Last 48 hours: (\d+)/", $data);
+
+		if( $day != 'n/a' )
+		{
+			$props['snow.daily'] = "Fresh($day)";
+			$props['snow.fresh'] = $day;
+		}
+
+		if( $yesterday != 'n/a' && $day != 'n/a' )
+			$props['snow.daily'] .= " 48hr($yesterday)";
+		else if( $yesterday != 'n/a' )
+			$props['snow.daily'] = "48hr($yesterday)";
+
+		$props['snow.units'] = 'inches';
+
+		$props['snow.total'] = find_int("/Mid Mountain Depth: (\d+)/", $data);
+
+		preg_match_all("/Lifts Open: (\d+)\/(\d+)/", $data, $matches, PREG_OFFSET_CAPTURE);
+		if($matches[1][0][0])
+		{
+			$props['lifts.open'] = $matches[1][0][0];
+			$props['lifts.total'] = $matches[2][0][0];
+		}
+		else
+		{
+			preg_match_all("/Lifts Open: (\d+)/", $data, $matches, PREG_OFFSET_CAPTURE);
+			if($matches[1][0][0])
+				$props['lifts.open'] = $matches[1][0][0];
+		}
+
+		preg_match_all("/Surface Conditions: (.*?)<br/", $data, $matches, PREG_OFFSET_CAPTURE);
+		if( $matches[1][0][0] )
+			$props['snow.conditions'] = $matches[1][0][0];
+
+		preg_match_all("/Comments:\s+(.*)/", $data, $matches, PREG_OFFSET_CAPTURE);
+		if( $matches[1][0][0] )
+		{
+			$props['location.comments'] = strip_tags($matches[1][0][0]);
+			//ensure we don't just give an empty comment
+			if( $props['location.comments'] == '--' )
+				unset($props['location.comments']);
+		}
+
+		$date = $report->getElementsByTagName('pubDate')->item(0)->nodeValue;
+		$date = strtotime($date);
+		$props['date'] = date("h:ia M j", $date);
+
+		$props['details.url'] = $report->getElementsByTagName('link')->item(0)->nodeValue;
+
+		return $props;
+	}
+
+	/**
+	 * Returns the XML node containing the report for a given location or false
+	 * if one is not found
+	 */
+	static function get_location_report($resort)
+	{
+		$dom = self::get_report_xml($resort);
+
+		$items = $dom->getElementsByTagName('item');
+		for ($i = 0; $i < $items->length; $i++)
+		{
+			$title_node = $items->item($i)->getElementsByTagName('title')->item(0);
+			$title = trim($title_node->firstChild->nodeValue);
+			$name = $resort->name;
+			if(preg_match("/$name/", $title) )
+			{
+			    return $items->item($i);
+			}
+		}
+
+		return false;
+	}
+
+	static function get_report_xml($resort)
+	{
+		$xml = file_get_contents($resort->fresh_source_url);
+		$sxe = simplexml_load_string($xml);
+		return dom_import_simplexml($sxe);
+	}
+}
+$report_class = 'COReport';
+ReportBase::run_cgi($report_class);
 ?>
