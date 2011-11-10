9c866c33b0c06531059eb2a17a45a2cb60130fa3
Andy Doan <doanac@gmail.com>
2011-11-09 18:16:05 -0600

    convert wy_jh_report to reportbase

--- wy_jh_report.php
+++ wy_jh_report.php
@@ -30,103 +30,99 @@
  * This script finds ski conditions for Wyoming Jackson Hole resort
  */
 require_once('wy.inc');
+require_once('reportbase.inc');
 
-header( "Content-Type: text/plain" );
-
-	$location = $_GET['location'];
-
-	$resorts = resorts_wy_get();
-	$resort = resort_get_location($resorts, $location);
-
-	$resort->fresh_source_url = "http://www.jacksonhole.com/mountain-info/conditions/weather-snow-report.html";
-	
-	$cache_file = 'wy_'.$location.'.txt';
-	$found_cache = cache_available($resort,$cache_file);
-	if( !$found_cache )
+class WYReportJH extends ReportBase
+{
+	public function run($location)
 	{
-		write_report($resort, $cache_file);
+		$resorts = resorts_wy_get();
+		$resort = resort_get_location($resorts, $location);
+
+		$resort->fresh_source_url = "http://www.jacksonhole.com/mountain-info/conditions/weather-snow-report.html";
+
+		$cache_file = 'wy_'.$location.'.txt';
+		$found_cache = cache_available($resort,$cache_file);
+		if( !$found_cache )
+		{
+			$this->write_report($resort, $cache_file);
+		}
+
+		cache_dump($cache_file, $found_cache);
+
+		log_hit('wy_jackson_hole_report.php', $location, $found_cache);
 	}
 
-	cache_dump($cache_file, $found_cache);
-
-	log_hit('wy_jackson_hole_report.php', $location, $found_cache);
-	
-function write_report($resort, $cache_file)
-{
-	$contents = get_location_content($resort);
-	if( $contents )
-	{
-		$props = get_resort_props($contents);
-		cache_create($resort, $cache_file, $props);
-	}
-}
-
-/*
- * Find a snow value given the name of the table row and the content of
- * the html page.  Returns n\a if table row or value was not found.
- */
-function find_snow_value($row_name,$content)
-{
 	/*
-	 * Snow totals are in table rows with <h6> headings based on row names.  
-	 * Snow values (for summit) follow with <h2> heading
+	 * Find a snow value given the name of the table row and the content of
+	 * the html page.  Returns n\a if table row or value was not found.
 	 */
-	$total = grep_grep_int("/<td.*?><h6>".$row_name."(.*?)<\/tr>/s","/<h2>(\d+)/",$content);
-	return $total;
+	static function find_snow_value($row_name,$content)
+	{
+		/*
+		 * Snow totals are in table rows with <h6> headings based on row names.
+		 * Snow values (for summit) follow with <h2> heading
+		 */
+		$total = grep_grep_int("/<td.*?><h6>".$row_name."(.*?)<\/tr>/s","/<h2>(\d+)/",$content);
+		return $total;
+	}
+
+	static function find_lifts_trails_open($desc,$content,&$matches)
+	{
+		return	preg_match("/<h2>(\d+)<\/h2>[^>]*>of (\d+)[^>]*>[^>]*>[^>]*>[^>]*>".$desc."/s",$content,$matches);
+	}
+
+	function get_report($resort)
+	{
+		$props = array();
+
+		$content = self::download($resort);
+
+		$props['snow.units'] = 'inches';
+
+		if(preg_match("/Weather \/ Snow Report<\/h1><[^>]+>[^\w]*([^<]+)<small>.*Reported as of:\s*(\d+:\d+\s*[A-Z][A-Z])/s", $content, $matches))
+		{
+			$props['date'] = $matches[2]." ".$matches[1];
+		}
+
+		$props['snow.fresh'] = self::find_snow_value("Since lifts closed",$content);
+		$props['snow.total'] = self::find_snow_value("Snow Depth", $content);
+		$value = self::find_snow_value("24 Hours",$content);
+		$props['snow.daily'] = "";
+		if( int_found($value) )
+		{
+			$props['snow.daily'] .= "24 Hours(" . $value . ")";
+		}
+		$value = self::find_snow_value("48 Hours",$content);
+		if( int_found($value) )
+		{
+			$props['snow.daily'] .= "48 Hours(" . $value . ")";
+		}
+
+		if( self::find_lifts_trails_open("Lifts Open",$content,$matches) )
+		{
+			$props['lifts.open'] = $matches[1];
+			$props['lifts.total'] = $matches[2];
+		}
+
+		if( self::find_lifts_trails_open("Trails Open",$content, $matches) )
+		{
+			$props['trails.open'] = $matches[1];
+			$props['trails.total'] = $matches[2];
+		}
+
+		return $props;
+	}
+
+	/**
+	 * Extracts the report content HTML for the given resort from the web page
+	 */
+	function download($resort)
+	{
+		$contents = file_get_contents($resort->fresh_source_url);
+		return $contents;
+	}
 }
-
-function find_lifts_trails_open($desc,$content,&$matches)
-{
-	return	preg_match("/<h2>(\d+)<\/h2>[^>]*>of (\d+)[^>]*>[^>]*>[^>]*>[^>]*>".$desc."/s",$content,$matches);
-}
-
-/**
- * Takes the resort HTML's content and builds its properties
- */
-function get_resort_props($content)
-{
-	$props = array();
-
-	$props['snow.units'] = 'inches';
-
-	if(preg_match("/Weather \/ Snow Report<\/h1><[^>]+>[^\w]*([^<]+)<small>.*Reported as of:\s*(\d+:\d+\s*[A-Z][A-Z])/s", $content, $matches)) 
-	{
-		$props['date'] = $matches[2]." ".$matches[1];
-	}
-
-	$props['snow.fresh'] = find_snow_value("Since lifts closed",$content);
-	$props['snow.total'] = find_snow_value("Snow Depth", $content);
-	$value = find_snow_value("24 Hours",$content);
-	if( int_found($value) ) 
-	{
-		$props['snow.daily'] .= "24 Hours(" . $value . ")";
-	}
-	$value = find_snow_value("48 Hours",$content);
-	if( int_found($value) ) 
-	{
-		$props['snow.daily'] .= "48 Hours(" . $value . ")";
-	}
-	
-	if( find_lifts_trails_open("Lifts Open",$content,$matches) )
-	{
-		$props['lifts.open'] = $matches[1];
-		$props['lifts.total'] = $matches[2];
-	}
-
-	if(find_lifts_trails_open("Trails Open",$content, $matches) )
-	{
-		$props['trails.open'] = $matches[1];
-		$props['trails.total'] = $matches[2];
-	}
-
-	return $props;
-}
-
-/**
- * Extracts the report content HTML for the given resort from the web page
- */
-function get_location_content($resort)
-{
-	$contents = file_get_contents($resort->fresh_source_url);
-	return $contents;
-}
+$report_class = 'WYReportJH';
+ReportBase::run_cgi($report_class);
+?>
