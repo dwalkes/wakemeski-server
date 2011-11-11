4342aeac3e11b0f1ffb9ddc5ea48fe318e17d7f6
Andy Doan <doanac@gmail.com>
2011-11-10 23:33:57 -0600

    convert whistler to reportbase

--- bc_whistler_report.php
+++ bc_whistler_report.php
@@ -27,141 +27,134 @@
  */
 
 require_once('bc.inc');
+require_once('reportbase.inc');
 
-header( "Content-Type: text/plain" );
-
-	$location = $_GET['location'];
-
-	$resorts = resorts_bc_get();
-	$resort = resort_get_location($resorts, $location);
-	
-	$cache_file = 'bc_'.$location.'.txt';
-	$found_cache = cache_available($resort,$cache_file);
-	if( !$found_cache )
+class BCReportWhis extends ReportBase
+{
+	public function run($location)
 	{
-		write_report($resort, $cache_file);
+		$resorts = resorts_bc_get();
+		$resort = resort_get_location($resorts, $location);
+
+		$cache_file = 'bc_'.$location.'.txt';
+		$found_cache = cache_available($resort,$cache_file);
+		if( !$found_cache )
+		{
+			$this->write_report($resort, $cache_file);
+		}
+
+		cache_dump($cache_file, $found_cache);
+
+		log_hit('bc_whistler_report.php', $location, $found_cache);
 	}
 
-	cache_dump($cache_file, $found_cache);
-
-	log_hit('bc_whistler_report.php', $location, $found_cache);
-
-function write_report($resort, $cache_file)
-{
-	$content = get_report($resort);
-	if( $content )
+	function get_report($resort)
 	{
-		$props = get_report_props($content);
-		cache_create($resort, $cache_file, $props);
-	}
-}
+		$props = array();
 
-/**
- * Takes the report's HTML and parse the information out into a hashtable
- */
-function get_report_props($report)
-{
-	$props = array();
+		$report = self::download($resort);
 
-	$props['snow.units'] = 'cm';
+		$props['snow.units'] = 'cm';
 
-	preg_match("/Last Updated:\s+(.*?)</", $report, $matches);
-	//this string can have odd padding in between words, so stript it off
-	$fields = preg_split("/\s+/", $matches[1]);
-	$props['date'] = implode(' ', $fields);
+		preg_match("/Last Updated:\s+(.*?)</", $report, $matches);
+		//this string can have odd padding in between words, so stript it off
+		$fields = preg_split("/\s+/", $matches[1]);
+		$props['date'] = implode(' ', $fields);
 
-	preg_match("/lline\">(\d+) cm(.*?)(\d+) cm(.*?)(\d+) cm(.*?)(\d+) cm(.*?)(\d+)/", $report, $matches);
-	$new  = $matches[1];
-	$hr24 = $matches[3];	
-	$hr48 = $matches[5];	
-	$week = $matches[7];	
-	$base = $matches[9];
+		preg_match("/lline\">(\d+) cm(.*?)(\d+) cm(.*?)(\d+) cm(.*?)(\d+) cm(.*?)(\d+)/", $report, $matches);
+		$new  = $matches[1];
+		$hr24 = $matches[3];
+		$hr48 = $matches[5];
+		$week = $matches[7];
+		$base = $matches[9];
 
-	$props['snow.fresh'] = $hr24;
-	$props['snow.daily'] = "Fresh($new) 24hr($hr24) 48hr($hr48) week($week)";
+		$props['snow.fresh'] = $hr24;
+		$props['snow.daily'] = "Fresh($new) 24hr($hr24) 48hr($hr48) week($week)";
 
-	$props['snow.total'] = $base;
+		$props['snow.total'] = $base;
 
-	preg_match("/Peak<(.*?)width=\"25%\">(.*?)&deg;/", $report, $matches);
-	$tempPeak = $matches[2];
-	preg_match("/Alpine<\/td><td>(.*?)&deg;/", $report, $matches);
-	$tempAlpine = $matches[1];
-	preg_match("/Village<\/td><td>(.*?)&deg;/", $report, $matches);
-	$tempVillage = $matches[1];
-	$props['temp.readings'] = $tempVillage.' '.$tempAlpine.' '.$tempPeak;
+		preg_match("/Peak<(.*?)width=\"25%\">(.*?)&deg;/", $report, $matches);
+		$tempPeak = $matches[2];
+		preg_match("/Alpine<\/td><td>(.*?)&deg;/", $report, $matches);
+		$tempAlpine = $matches[1];
+		preg_match("/Village<\/td><td>(.*?)&deg;/", $report, $matches);
+		$tempVillage = $matches[1];
+		$props['temp.readings'] = $tempVillage.' '.$tempAlpine.' '.$tempPeak;
 
-	get_weather_props(&$props);	
+		self::get_weather_props(&$props);
 
-	return $props;
-}
-
-// kind of a hack. we don't get exact timestamps so we hack something
-function get_weather_exact($label)
-{
-	if($label == "Tonight")
-	{
-		//get the next midnight
-		return strtotime("12:00am") + (24*60*60);
-	}
-	else
-	{
-		// these will be days of week like Monday. strtotime
-		// returns the time at midnight. We add 6 hours so that
-		// it will look like "Monday 6:00 am" in the app
-		return strtotime($label) + (6*60*60);
-	}
-}
-
-function get_weather_interval($idx, $label, $offset, $props, $contents)
-{
-	$props['weather.forecast.when.'.$idx] = $label;
-	$props['weather.forecast.when-exact.'.$idx] = get_weather_exact($label);
-
-	//move to the offset for this forecast
-	$contents = substr($contents, $offset);
-
-	if($idx == 1)
-	{
-		preg_match("/weathericons\/(.*?).gif/", $contents, $matches);
-		$props['weather.icon'] = bc_get_weather_icon($matches[1]);
+		return $props;
 	}
 
-	preg_match("/title='(.*?)'>/", $contents, $matches);
-	$props['weather.forecast.desc.'.$idx] = $matches[1];
+	// kind of a hack. we don't get exact timestamps so we hack something
+	static function get_weather_exact($label)
+	{
+		if($label == "Tonight")
+		{
+			//get the next midnight
+			return strtotime("12:00am") + (24*60*60);
+		}
+		else
+		{
+			// these will be days of week like Monday. strtotime
+			// returns the time at midnight. We add 6 hours so that
+			// it will look like "Monday 6:00 am" in the app
+			return strtotime($label) + (6*60*60);
+		}
+	}
+
+	static function get_weather_interval($idx, $label, $offset, $props, $contents)
+	{
+		$props['weather.forecast.when.'.$idx] = $label;
+		$props['weather.forecast.when-exact.'.$idx] = self::get_weather_exact($label);
+
+		//move to the offset for this forecast
+		$contents = substr($contents, $offset);
+
+		if($idx == 1)
+		{
+			preg_match("/weathericons\/(.*?).gif/", $contents, $matches);
+			$props['weather.icon'] = bc_get_weather_icon($matches[1]);
+		}
+
+		preg_match("/title='(.*?)'>/", $contents, $matches);
+		$props['weather.forecast.desc.'.$idx] = $matches[1];
+	}
+
+	static function get_weather_props($props)
+	{
+		//make sure this is Pacific time in case the PHP server is another timezone
+		//otherwise the strtotime functions won't work for this
+		date_default_timezone_set('America/Los_Angeles');
+
+		$props['weather.url'] = "http://movement.whistlerblackcomb.com/cache/whistler_fx.php";
+		$contents = file_get_contents($props['weather.url']);
+
+		//get the text to parse
+		$idx1 = strpos($contents, "<table width='100%' cellpadding='0' cellspacing='0' class='icons' >");
+		$idx2 = strpos($contents, "</table>", $idx1);
+		$contents = substr($contents, $idx1, $idx2-$idx1);
+
+		//split this up based on each interval (ie Sunday, Monday, Tuesday)
+		// keep the offset's so we can parse the actual report
+		preg_match_all("/<span class='titleS'>(.*?)<\/span>/", $contents, $matches, PREG_OFFSET_CAPTURE);
+
+		self::get_weather_interval(0, $matches[1][0][0], $matches[1][0][1], &$props, $contents);
+		self::get_weather_interval(1, $matches[1][1][0], $matches[1][1][1], &$props, $contents);
+		self::get_weather_interval(2, $matches[1][2][0], $matches[1][2][1], &$props, $contents);
+		self::get_weather_interval(3, $matches[1][3][0], $matches[1][3][1], &$props, $contents);
+	}
+
+	static function download($resort)
+	{
+		$contents = file_get_contents($resort->fresh_source_url);
+
+		//strip off some the junk we don't need
+		$contents = strstr($contents, "Last Updated");
+		$idx = strpos($contents, ">Grooming</div>");
+		return(substr($contents, 0, $idx));
+	}
 }
-
-function get_weather_props($props)
-{
-	//make sure this is Pacific time in case the PHP server is another timezone
-	//otherwise the strtotime functions won't work for this
-	date_default_timezone_set('America/Los_Angeles');
-
-	$props['weather.url'] = "http://movement.whistlerblackcomb.com/cache/whistler_fx.php";
-	$contents = file_get_contents($props['weather.url']);
-
-	//get the text to parse
-	$idx1 = strpos($contents, "<table width='100%' cellpadding='0' cellspacing='0' class='icons' >");
-	$idx2 = strpos($contents, "</table>", $idx1);
-	$contents = substr($contents, $idx1, $idx2-$idx1);
-
-	//split this up based on each interval (ie Sunday, Monday, Tuesday)
-	// keep the offset's so we can parse the actual report
-	preg_match_all("/<span class='titleS'>(.*?)<\/span>/", $contents, $matches, PREG_OFFSET_CAPTURE);
-
-	get_weather_interval(0, $matches[1][0][0], $matches[1][0][1], &$props, $contents);
-	get_weather_interval(1, $matches[1][1][0], $matches[1][1][1], &$props, $contents);
-	get_weather_interval(2, $matches[1][2][0], $matches[1][2][1], &$props, $contents);
-	get_weather_interval(3, $matches[1][3][0], $matches[1][3][1], &$props, $contents);
-}
-
-function get_report($resort)
-{
-	$contents = file_get_contents($resort->fresh_source_url);
-
-	//strip off some the junk we don't need
-	$contents = strstr($contents, "Last Updated");
-	$idx = strpos($contents, ">Grooming</div>");
-	return(substr($contents, 0, $idx));
-}
-
+$report_class = 'BCReportWhis';
+ReportBase::run_cgi($report_class);
 ?>
