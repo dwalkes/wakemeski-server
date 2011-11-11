ee2073d8116c255ba584690ce6e46f3be08a4a11
Andy Doan <doanac@gmail.com>
2011-11-10 23:20:41 -0600

    convert bc_revelstoke over

--- bc_revelstoke_report.php
+++ bc_revelstoke_report.php
@@ -27,113 +27,106 @@
  */
 
 require_once('bc.inc');
+require_once('reportbase.inc');
 
-header( "Content-Type: text/plain" );
-
-	$location = $_GET['location'];
-
-	$resorts = resorts_bc_get();
-	$resort = resort_get_location($resorts, $location);
-
-	$resort->fresh_source_url = $resort->data;
-
-	$cache_file = 'bc_'.$location.'.txt';
-	$found_cache = cache_available($resort,$cache_file);
-	if( !$found_cache )
+class BCReportRev extends ReportBase
+{
+	public function run($location)
 	{
-		write_report($resort, $cache_file);
+		$resorts = resorts_bc_get();
+		$resort = resort_get_location($resorts, $location);
+
+		$resort->fresh_source_url = $resort->data;
+
+		$cache_file = 'bc_'.$location.'.txt';
+		$found_cache = cache_available($resort,$cache_file);
+		if( !$found_cache )
+		{
+			$this->write_report($resort, $cache_file);
+		}
+
+		cache_dump($cache_file, $found_cache);
+
+		log_hit('bc_revelstoke_report.php', $location, $found_cache);
 	}
 
-	cache_dump($cache_file, $found_cache);
-
-	log_hit('bc_revelstoke_report.php', $location, $found_cache);
-
-function write_report($resort, $cache_file)
-{
-	$content = get_report($resort);
-	if( $content )
+	function get_report($resort)
 	{
-		$props = get_report_props($resort, $content);
-		cache_create($resort, $cache_file, $props);
+		$props = array();
+
+		$report = self::download($resort);
+
+		$props['snow.units'] = 'cm';
+
+		$props['details.url'] = $resort->fresh_source_url;
+
+		preg_match("/Timestamp: (.*?)</", $report, $matches);
+		$props['date'] = $matches[1];
+
+		preg_match_all("/emph'>(.*?)\s+cm/", $report, $matches);
+
+		$props['snow.fresh'] = $matches[1][0];
+		$props['snow.daily'] = "Hourly(".$matches[1][1].") 24hr(".$matches[1][0].")";
+
+		$props['snow.total'] = $matches[1][2];
+
+		preg_match("/'giant'>(.*?)° C/", $report, $matches);
+		$props['temp.readings'] = $matches[1];
+
+		//TODO? $props['trails.open'] = $matches[2];
+		//TODO? $props['trails.total'] = $matches[3];
+
+		self::get_weather_props(&$props);
+
+		return $props;
+	}
+
+	static function get_weather_props($props)
+	{
+		$props['weather.url'] = "http://www.weatheroffice.gc.ca/city/pages/bc-65_metric_e.html";
+		$report = self::get_weather_report($props);
+
+		self::get_weather_icon($report, &$props);
+
+		//remove the contents up to the actual weather so that we can get
+		//the proper list:
+		$report = strstr($report, "<div class=\"fdetails\">");
+
+		preg_match_all("/<dt>(.*?)<\/dt>\s+<dd>(.*?)<\/dd>/", $report, $matches);
+
+		for($i = 0; $i < 3; $i++)
+		{
+			$props['weather.forecast.when.'.$i] = $matches[1][$i];
+			$props['weather.forecast.desc.'.$i] = $matches[2][$i];
+		}
+	}
+
+	static function get_weather_icon($report, $props)
+	{
+		if(preg_match("/id=\"currentimg\" src=\"\/weathericons\/(.*?).gif/", $report, $matches) )
+			$props['weather.icon'] = bc_get_weather_icon($matches[1]);
+	}
+
+	static function get_weather_report($props)
+	{
+		$contents = file_get_contents($props['weather.url']);
+
+		//its easier to parse if one line
+		return str_replace("\n", "\t", $contents);
+	}
+
+	static function download($resort)
+	{
+		$contents = file_get_contents($resort->fresh_source_url);
+		$idx = strpos($contents, "Timestamp:");
+		if( $idx !== false )
+		{
+			$idx2 = strpos($contents, "</div>", $idx);
+			$contents = substr($contents, $idx, $idx2-$idx);
+		}
+		return $contents;
 	}
 }
-
-/**
- * Takes the report's HTML and parse the information out into a hashtable
- */
-function get_report_props($resort, $report)
-{
-	$props = array();
-
-	$props['snow.units'] = 'cm';
-
-	$props['details.url'] = $resort->fresh_source_url;
-
-	preg_match("/Timestamp: (.*?)</", $report, $matches);
-	$props['date'] = $matches[1];
-
-	preg_match_all("/emph'>(.*?)\s+cm/", $report, $matches);
-
-	$props['snow.fresh'] = $matches[1][0];
-	$props['snow.daily'] = "Hourly(".$matches[1][1].") 24hr(".$matches[1][0].")";
-
-	$props['snow.total'] = $matches[1][2];
-
-	preg_match("/'giant'>(.*?)° C/", $report, $matches);
-	$props['temp.readings'] = $matches[1];
-
-	//TODO? $props['trails.open'] = $matches[2];
-	//TODO? $props['trails.total'] = $matches[3];
-
-	get_weather_props(&$props);
-
-	return $props;
-}
-
-function get_weather_props($props)
-{
-	$props['weather.url'] = "http://www.weatheroffice.gc.ca/city/pages/bc-65_metric_e.html";
-	$report = get_weather_report($props);
-
-	get_weather_icon($report, &$props);
-
-	//remove the contents up to the actual weather so that we can get
-	//the proper list:
-	$report = strstr($report, "<div class=\"fdetails\">");
-
-	preg_match_all("/<dt>(.*?)<\/dt>\s+<dd>(.*?)<\/dd>/", $report, $matches);
-
-	for($i = 0; $i < 3; $i++)
-	{
-		$props['weather.forecast.when.'.$i] = $matches[1][$i];
-		$props['weather.forecast.desc.'.$i] = $matches[2][$i];
-	}
-}
-
-function get_weather_icon($report, $props)
-{
-	if(preg_match("/id=\"currentimg\" src=\"\/weathericons\/(.*?).gif/", $report, $matches) )
-		$props['weather.icon'] = bc_get_weather_icon($matches[1]);
-}
-
-function get_weather_report($props)
-{
-	$contents = file_get_contents($props['weather.url']);
-
-	//its easier to parse if one line
-	return str_replace("\n", "\t", $contents);
-}
-
-function get_report($resort)
-{
-	$contents = file_get_contents($resort->fresh_source_url);
-	$idx = strpos($contents, "Timestamp:");
-	if( $idx !== false )
-	{
-		$idx2 = strpos($contents, "</div>", $idx);
-		$contents = substr($contents, $idx, $idx2-$idx);
-	}
-	return $contents;
-}
-
+$report_class = 'BCReportRev';
+ReportBase::run_cgi($report_class);
 ?>
